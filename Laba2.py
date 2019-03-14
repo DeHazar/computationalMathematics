98b529f34fb07ca4ef7a2b35ede1dfea5696216a
Garaev Denis <garaev.denchik@gmail.com>
2019-03-14 15:26:01 +0500

    Finish Laba1 Реализовал метод левых прямоугольников Метод Симметрич¬ных прямоугольников

--- Laba2.py
+++ Laba2.py
@@ -1,25 +1,35 @@
 def calculateIntegral(fun, a: float, b: float, degree: int, n: int = 1000):
     """
-
-    :param fun: Функция от которой необходимо вычислить
+    Метод симметричных прямоугольников
+    :param fun: Функция от которой необходимо вычислить,  вид а y = m*x^degree
     :param a: Верхний предел интегрирования
     :param b: Нижний предел
     :param n: Количество разбиений
+    :param degree: Степень при функции
     :return:
     """
     reachEps = False
     while not reachEps:
         h = (b - a) / n
         x = [(i * h + a) for i in range(n)]
-        M1 = (b - a) * max([degree * fun(x[i], degree - 1) for i in range(n)])
-        sum = 0
-        if 10 ** -5 <= M1 * h / 2:
-            n = n + 10000
-            continue
+        M2 = (b - a) * max([degree*(degree-1) * fun(x[i], degree - 2) for i in range(n)])
+        sumH = 0
+        sumHalfH = 0
+
         for i in range(n - 1):
-            sum += h * fun(x[i], degree)
-        return sum
+            sumH += h * fun(x[i]+h/2, degree)
+            sumHalfH += h * fun(x[i]+h/2, degree)/2
+
+        deltaRunge = (sumHalfH - sumH)/(2 ** 2 - 1)
+
+        if not deltaRunge <= 10 ** -6:
+            n = 2*n
+            continue
+        # if 10 ** -6 <= M2 * h**2 / 24:
+        #     n = 2*n
+        #     continue
+        return sumH
 
 
 if __name__ == '__main__':
-    print(calculateIntegral(lambda x, m: x ** m, a=0, b=2, degree=4, n=2))
+    print(calculateIntegral(lambda x, m: 6*x ** m, a=0, b=1, degree=5, n=2))
